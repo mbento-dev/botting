9438c09667018314d151fc58c94401a14b41240a
Bento <mbento.bauru@gmail.com>
2024-10-25 18:41:56 -0300

    feat: start chinconpa bot

---
+++ src/main/java/com/runemate/fenrisfeng/chompers/ChinChompers.java
@@ -0,0 +1,11 @@
+package com.runemate.fenrisfeng.chompers;
+
+import com.runemate.game.api.script.framework.tree.*;
+
+public class ChinChompers extends TreeBot {
+
+    @Override
+    public TreeTask createRootTask() {
+        return null;
+    }
+}
