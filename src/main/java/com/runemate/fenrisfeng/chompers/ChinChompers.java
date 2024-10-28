51b189b73bd53a3c004c2382f869c17373f4985a
Bento <mbento.bauru@gmail.com>
2024-10-27 22:31:53 -0300

    feat: change chin bot to task bot

--- src/main/java/com/runemate/fenrisfeng/chompers/ChinChompers.java
+++ src/main/java/com/runemate/fenrisfeng/chompers/ChinChompers.java
@@ -1,11 +1,9 @@
 package com.runemate.fenrisfeng.chompers;
 
+import com.runemate.fenrisfeng.chompers.tasks.*;
+import com.runemate.game.api.script.framework.task.*;
 import com.runemate.game.api.script.framework.tree.*;
 
-public class ChinChompers extends TreeBot {
+public class ChinChompers extends TaskBot {
 
-    @Override
-    public TreeTask createRootTask() {
-        return null;
-    }
 }
