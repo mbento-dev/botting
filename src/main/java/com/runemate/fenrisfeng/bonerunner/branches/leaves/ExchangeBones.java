b03c0755aa526821453a5e6b7aad503faa95ab1d
Bento <mbento.bauru@gmail.com>
2024-10-29 17:38:56 -0300

    feat: use settings on CheckForBones

--- src/main/java/com/runemate/fenrisfeng/bonerunner/branches/leaves/ExchangeBones.java
+++ src/main/java/com/runemate/fenrisfeng/bonerunner/branches/leaves/ExchangeBones.java
@@ -1,20 +1,23 @@
 package com.runemate.fenrisfeng.bonerunner.branches.leaves;
 
+import com.runemate.fenrisfeng.bonerunner.*;
 import com.runemate.game.api.hybrid.*;
 import com.runemate.game.api.hybrid.entities.*;
 import com.runemate.game.api.hybrid.local.*;
 import com.runemate.game.api.hybrid.local.hud.interfaces.*;
+import com.runemate.game.api.hybrid.location.*;
 import com.runemate.game.api.hybrid.region.*;
 import com.runemate.game.api.script.*;
 import com.runemate.game.api.script.framework.tree.*;
-import lombok.*;
+import java.util.*;
 
 public class ExchangeBones extends LeafTask {
-    @Setter
-    private String boneType = "Dragon bones";
+    private final BoneRunner bot = (BoneRunner) Objects.requireNonNull(Environment.getBot());
 
     @Override
     public void execute() {
+        String boneType = bot.bonesSettings.boneType();
+
         SpriteItem notedBones = Inventory
             .newQuery()
             .names(boneType)
@@ -22,13 +25,17 @@ public class ExchangeBones extends LeafTask {
             .results()
             .first();
         if (notedBones == null) {
-            Environment.getBot().pause("No more " + boneType + " found.");
+            bot.pause("No more " + boneType + " found.");
             return;
         }
 
         Npc phials = Npcs
             .newQuery()
             .names("Phials")
+            .within(new Area.Rectangular(
+                new Coordinate(2945, 3219,0),
+                new Coordinate(2951,3210, 0)
+            ))
             .results()
             .first();
         if (phials == null) {
@@ -36,8 +43,16 @@ public class ExchangeBones extends LeafTask {
         }
 
         notedBones.click();
+
         Camera.concurrentlyTurnTo(phials);
         Execution.delayUntil(phials::isVisible);
+
         phials.click();
+
+        ChatDialog.Option dialogOpt = ChatDialog.getOption("Exchange All: \\d* coins");
+        if (dialogOpt == null){
+            return;
+        }
+        dialogOpt.select();
     }
 }
