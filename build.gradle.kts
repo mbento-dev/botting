49d61db4b31b18be1addaee8b9a86469b6e9e453
Bento <mbento.bauru@gmail.com>
2024-10-29 17:37:29 -0300

    feat: prepare build.gradle.kts for tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,9 +14,9 @@ runemate {
     devMode.set(true)
 
     manifests {
-        create("Sandstorm") {
+        create("Fenris Bone Runner") {
         //This is the fully qualified name of your main class
-        mainClass = "com.runemate.fenrisfeng.sandstorm.Sandstorm"
+        mainClass = "com.runemate.fenrisfeng.bonerunner.BoneRunner"
 
         //A short description that is shown under the bot name on the bot store
         tagline = "a test bot :)"
@@ -31,7 +31,7 @@ runemate {
         version = "0.0.1"
 
         //The store supports multiple categories, the first will be the "main" category
-        categories(Category.MINING)
+        categories(Category.OTHER)
 
         //This is where you declare the price(s) of the bot
         variants {
@@ -44,66 +44,6 @@ runemate {
 //                allowance = Duration.ofHours(3)
 //            }
     }
-        create("Hunleff Mate") {
-            //This is the fully qualified name of your main class
-            mainClass = "com.runemate.fenrisfeng.hunleffmate.HunleffMate"
-
-            //A short description that is shown under the bot name on the bot store
-            tagline = "a test bot :)"
-
-            //Shown in the bot description in the client
-            description = "Tests stuff?"
-
-            //The unique internal ID of the bot
-            internalId = "test-bot"
-
-            //The version of the bot
-            version = "0.0.1"
-
-            //The store supports multiple categories, the first will be the "main" category
-            categories(Category.BOSSING)
-
-            //This is where you declare the price(s) of the bot
-            variants {
-                variant(name = "Variant name", price = 0.00)
-            }
-
-            //For premium bots, you can declare a "trial" which is a period for which a user can use the bot for free
-//            trial {
-//                window = Duration.ofDays(7)
-//                allowance = Duration.ofHours(3)
-//            }
-        }
-        create("Nagua Farmer") {
-            //This is the fully qualified name of your main class
-            mainClass = "com.runemate.fenrisfeng.nagua.NaguaFarmer"
-
-            //A short description that is shown under the bot name on the bot store
-            tagline = "A test bot to farm nagua for exp"
-
-            //Shown in the bot description in the client
-            description = "Tests stuff?"
-
-            //The unique internal ID of the bot
-            internalId = "nagua-3000"
-
-            //The version of the bot
-            version = "0.0.4"
-
-            //The store supports multiple categories, the first will be the "main" category
-            categories(Category.COMBAT)
-
-            //This is where you declare the price(s) of the bot
-            variants {
-                variant(name = "Variant name", price = 0.00)
-            }
-
-            //For premium bots, you can declare a "trial" which is a period for which a user can use the bot for free
-//            trial {
-//                window = Duration.ofDays(7)
-//                allowance = Duration.ofHours(3)
-//            }
-        }
     }
 
 }
