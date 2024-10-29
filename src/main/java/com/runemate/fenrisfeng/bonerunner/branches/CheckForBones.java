714d9e38761f2249687a0bf4195e00bb6389eb74
Bento <mbento.bauru@gmail.com>
2024-10-29 17:38:19 -0300

    feat: use settings on CheckForBones

--- src/main/java/com/runemate/fenrisfeng/bonerunner/branches/CheckForBones.java
+++ src/main/java/com/runemate/fenrisfeng/bonerunner/branches/CheckForBones.java
@@ -1,15 +1,14 @@
 package com.runemate.fenrisfeng.bonerunner.branches;
 
+import com.runemate.fenrisfeng.bonerunner.*;
+import com.runemate.game.api.hybrid.*;
 import com.runemate.game.api.hybrid.local.hud.interfaces.*;
 import com.runemate.game.api.hybrid.queries.results.*;
 import com.runemate.game.api.script.framework.tree.*;
-import lombok.*;
+import java.util.*;
 
 public class CheckForBones extends BranchTask {
-    @Setter
-    private String boneType = "Dragon bones";
-    @Setter
-    private int inventorySize = 27;
+    private final BoneRunner bot = (BoneRunner) Objects.requireNonNull(Environment.getBot());
 
     private final TreeTask onSuccessTask;
     private final TreeTask onFailureTask;
@@ -21,12 +20,15 @@ public class CheckForBones extends BranchTask {
 
     @Override
     public boolean validate() {
+        String boneType = bot.bonesSettings.boneType();
+        int bonesAmount = bot.bonesSettings.bonesAmount();
+
         SpriteItemQueryResults bones = Inventory
             .newQuery()
             .names(boneType)
             .unnoted()
             .results();
-        return bones.size() >= inventorySize;
+        return bones.size() >= bonesAmount;
     }
 
     @Override
