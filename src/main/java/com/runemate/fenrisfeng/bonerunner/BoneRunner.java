e1e44900d963f51388b842a6427c6c60a947a9a1
Bento <mbento.bauru@gmail.com>
2024-10-29 10:39:02 -0300

    feat: start BoneRunner request

---
+++ src/main/java/com/runemate/fenrisfeng/bonerunner/BoneRunner.java
@@ -0,0 +1,17 @@
+package com.runemate.fenrisfeng.bonerunner;
+
+import com.runemate.game.api.hybrid.*;
+import com.runemate.game.api.script.framework.tree.*;
+
+public class BoneRunner extends TreeBot {
+
+    @Override
+    public void onStart(final String... arguments) {
+
+    }
+
+    @Override
+    public TreeTask createRootTask() {
+        return null;
+    }
+}
---
+++ src/main/java/com/runemate/fenrisfeng/bonerunner/tasks/leaves/ExchangeBones.java
@@ -0,0 +1,43 @@
+package com.runemate.fenrisfeng.bonerunner.tasks.leaves;
+
+import com.runemate.game.api.hybrid.*;
+import com.runemate.game.api.hybrid.entities.*;
+import com.runemate.game.api.hybrid.local.*;
+import com.runemate.game.api.hybrid.local.hud.interfaces.*;
+import com.runemate.game.api.hybrid.region.*;
+import com.runemate.game.api.script.*;
+import com.runemate.game.api.script.framework.tree.*;
+import lombok.*;
+
+public class ExchangeBones extends LeafTask {
+    @Setter
+    private String boneType = "Dragon bones";
+
+    @Override
+    public void execute() {
+        SpriteItem notedBones = Inventory
+            .newQuery()
+            .names(boneType)
+            .noted()
+            .results()
+            .first();
+        if (notedBones == null) {
+            Environment.getBot().pause("No more " + boneType + " found.");
+            return;
+        }
+
+        Npc phials = Npcs
+            .newQuery()
+            .names("Phials")
+            .results()
+            .first();
+        if (phials == null) {
+            return;
+        }
+
+        notedBones.click();
+        Camera.concurrentlyTurnTo(phials);
+        Execution.delayUntil(phials::isVisible);
+        phials.click();
+    }
+}
